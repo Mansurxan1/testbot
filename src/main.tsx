8db0336d1d63d28797497d50f382ec27a765768b
Mansurxan <Mansurxan.uz>
2025-03-25 13:33:06 +0500

    test

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,16 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-import { store } from "./store/store";
+import {store} from "./store/store";
 import App from "./App";
-import "./i18n/i18n";
-import "./index.css";
 
-const rootElement = document.getElementById("root");
-if (rootElement) {
-  createRoot(rootElement).render(
-    <StrictMode>
-      <Provider store={store}>
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
         <App />
-      </Provider>
-    </StrictMode>
-  );
-}
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
